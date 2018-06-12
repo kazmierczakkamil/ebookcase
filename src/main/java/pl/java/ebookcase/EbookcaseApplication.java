b45fc13c41e0fba9bd1633cf811b3410d7aa3d86
Kamil <203901@edu.p.lodz.pl>
2018-06-12 13:08:54 +0200

    Initial commit

---
+++ src/main/java/pl/java/ebookcase/EbookcaseApplication.java
@@ -0,0 +1,12 @@
+package pl.java.ebookcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookcaseApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/java/ebookcase/EbookcaseApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.java.ebookcase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EbookcaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
